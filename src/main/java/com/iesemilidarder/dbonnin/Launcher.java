e054dc83e7b108821ea879205a2283aed5c09ac9
David <dbs00@iesemilidarder.com>
2018-10-04 17:55:14 +0200

    Clase

---
+++ src/main/java/com/iesemilidarder/dbonnin/Launcher.java
@@ -0,0 +1,14 @@
+package com.iesemilidarder.dbonnin;
+
+import com.iesemilidarder.dbonnin.data.Moto;
+
+public class Launcher {
+    public static void main(String... args) {
+        System.out.println("Concesionario brumbrum App 2.0");
+        Moto moto = new Moto();
+        moto.start();
+        System.out.println("pista que voy!");
+        moto.brake();
+    }
+}
+
---
+++ src/main/java/com/iesemilidarder/dbonnin/data/Moto.java
@@ -0,0 +1,8 @@
+package com.iesemilidarder.dbonnin.data;
+
+public class Moto extends Vehicle {
+
+    public void start() {
+        System.out.println("brum brum");
+    }
+}
