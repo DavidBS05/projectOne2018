3691cf52df936ad4d776a892aaea6af30df89951
David <dbs00@iesemilidarder.com>
2018-10-11 16:00:34 +0200

    Commit

--- src/main/java/com/iesemilidarder/dbonnin/Launcher.java
+++ src/main/java/com/iesemilidarder/dbonnin/Launcher.java
@@ -1,16 +1,22 @@
 package com.iesemilidarder.dbonnin;
 
 import com.iesemilidarder.dbonnin.data.Moto;
+import com.iesemilidarder.dbonnin.data.SystemUtilHelper;
 import com.iesemilidarder.dbonnin.data.Vehicle;
 
 public class Launcher {
     public static void main(String... args) {
-        System.out.println("Concesionario brumbrum App 2.0");
+        SystemUtilHelper.consolePrint("Concesionario brumbrum App 2.0");
         Moto moto = new Moto();
+        moto.setNumWheels(2);
+        moto.setPrice(123.45);
+        moto.setColor("Blanco");
+        System.out.println("Ruedas"+moto.getNumWheels());
         moto.start();
-        System.out.println("pista que voy!");
+        SystemUtilHelper.consolePrint("pista que voy!");
         moto.brake();
         Vehicle aux = new Moto();
+        SystemUtilHelper.consolePrint("yuhu!!");
     }
 }
 
---
+++ src/main/java/com/iesemilidarder/dbonnin/data/SystemUtilHelper.java
@@ -0,0 +1,12 @@
+package com.iesemilidarder.dbonnin.data;
+
+public class SystemUtilHelper {
+
+    private SystemUtilHelper() {
+        //Todo: OS gereralization
+    }
+
+    public static void consolePrint (String message) {
+        System.out.println(message);
+    }
+}
